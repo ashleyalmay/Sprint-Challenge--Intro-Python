844e94b2108153152f1977d4b2cae697a64a76fe
Ashley <54685244+ashleyalmay@users.noreply.github.com>
2020-07-10 13:56:43 -0400

    still working on it

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,32 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    pass
+#top level basic grandpartents 
+
+class FlightVehicle(Vehicle):
+    pass
+#takes parents diff from grandpartents
+
+class Airplane(FlightVehicle):
+    pass
+#takes parents diff from grandpartents child
+
+class Starship(FlightVehicle):
+    pass
+#takes parents diff from grandpartents child
+
+class GroundVehicle(Vehicle):
+    pass
+#takes parents 
+
+class Car(GroundVehicle):
+    pass
+#last level siblings
+
+class Motorcycle(GroundVehicle):
+    pass
+#last level siblings
+
+#wrote down on paper because the chart was confusing
--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -1,14 +1,17 @@
+from oop1 import Vehicle
+
 # To the GroundVehicle class, add method drive() that returns "vroooom".
 #
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
-class GroundVehicle():
-    def __init__(self, num_wheels):
+class GroundVehicle(Vehicle):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
-
+    # todo
+    def drive(self):
+        return 'vroooom'
 
 # Subclass Motorcycle from GroundVehicle.
 #
@@ -17,7 +20,12 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
-# TODO
+# todo
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(2)
+    def drive(self):
+        return "BRAAAP!!"
 
 vehicles = [
     GroundVehicle(),
@@ -29,4 +37,9 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+#todo
+for v in vehicles:
+    print(v.drive())
+
+
+#ref 15 classes and 11 args
